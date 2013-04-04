d3e2d15ee972b6626981d36b79c9880452464d8d
Christoph Reiter <reiter.christoph@gmail.com>
2013-04-04 19:52:56 +0200

    docs: enable intersphinx

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 import gi
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'pgidoc'
@@ -14,6 +14,8 @@ copyright = u'2013, Christoph Reiter'
 version = "0.1"
 release = "0.1"
 exclude_patterns = ['_build', 'README.rst']
+
+intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None)}
 
 html_theme_path = ['../']
 html_theme = 'minimalism'
