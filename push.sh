8c9fbe9f63b94a3e215bbd613f8013a2fe7b4168
Christoph Reiter <reiter.christoph@gmail.com>
2013-11-21 12:10:10 +0100

    Add desc to the created repo

--- push.sh
+++ push.sh
@@ -3,6 +3,9 @@
 # after ./default.sh, call this to force push the build to github pages
 
 cd _docs/_build
+rm -f README.rst
+echo "This repository was created automatically using https://github.com/lazka/pgi-docgen\n" >> README.rst
+echo "It contains a static website which can be viewed at http://lazka.github.io/pgi-docs/" >> README.rst
 rm -rf .?*
 git init
 git checkout -b gh-pages
