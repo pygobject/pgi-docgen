993ae82f858270dac02a91d7d5aa448a5ec477b3
Christoph Reiter <reiter.christoph@gmail.com>
2014-01-29 18:42:28 +0100

    Disable translations for introspected libs

--- pgidocgen/__init__.py
+++ pgidocgen/__init__.py
@@ -4,3 +4,9 @@
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
+
+import os
+
+# Force disable translations for gi libraries.
+# For example descriptions of properties/signals are translated.
+os.environ["LANG"] = "en"
