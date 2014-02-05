bd5c8a5ecb104b17f608b64d3f1526c12f5a7475
Christoph Reiter <reiter.christoph@gmail.com>
2014-02-05 01:50:22 +0100

    Add GOffice-0.10 to default build

--- build_all.sh
+++ build_all.sh
@@ -37,7 +37,7 @@
     PackageKitGlib-1.0 PackageKitPlugin-1.0 PanelApplet-4.0 \
     Peas-1.0 PeasGtk-1.0 Polkit-1.0 UPowerGlib-1.0 SecretUnstable-0 \
     PolkitAgent-1.0 Rest-0.7 RestExtras-0.7 Rsvg-2.0 UMockdev-1.0 \
-    SocialWebClient-0.25 SoupGNOME-2.4 SpiceClientGLib-2.0 \
+    SocialWebClient-0.25 SoupGNOME-2.4 SpiceClientGLib-2.0 GOffice-0.10 \
     SpiceClientGtk-3.0 SugarGestures-1.0 TelepathyGLib-0.12 Wnck-3.0 \
     TelepathyLogger-0.2 TotemPlParser-1.0 Xkl-1.0 Zpj-0.0 Zeitgeist-2.0 \
     && ./pgi-docgen-build.py _docs/_build _docs
