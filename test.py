ed08a7412b4f88bcbfaf21b02abedad095873a1a
Christoph Reiter <reiter.christoph@gmail.com>
2014-02-21 19:47:02 +0100

    tests: Add -x option

--- test.py
+++ test.py
@@ -7,17 +7,25 @@
 # version 2.1 of the License, or (at your option) any later version.
 
 import os
+import sys
 import unittest
+import argparse
 
 
-def do_test():
+def do_test(argv):
+    parser = argparse.ArgumentParser(description='Run tests')
+    parser.add_argument('-x', '--exitfirst', action='store_true',
+                        help="exit instantly on first error or failed test")
+    args = parser.parse_args(argv[1:])
+
     loader = unittest.TestLoader()
     current_dir = os.path.join(os.path.dirname(__file__))
     suites = loader.discover(os.path.join(current_dir, "tests"))
 
-    run = unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(suites))
+    run = unittest.TextTestRunner(
+        verbosity=2, failfast=args.exitfirst).run(unittest.TestSuite(suites))
     return len(run.failures) + len(run.errors)
 
 
 if __name__ == "__main__":
-    exit(do_test() != 0)
+    exit(do_test(sys.argv) != 0)
