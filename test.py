ae30075b8553f5df2badb848227aad46b15a4c73
Christoph Reiter <reiter.christoph@gmail.com>
2014-02-21 22:13:29 +0100

    tests: Add --filter option

--- test.py
+++ test.py
@@ -16,11 +16,25 @@ def do_test(argv):
     parser = argparse.ArgumentParser(description='Run tests')
     parser.add_argument('-x', '--exitfirst', action='store_true',
                         help="exit instantly on first error or failed test")
+    parser.add_argument('-f', '--filter', help="regexp")
     args = parser.parse_args(argv[1:])
 
     loader = unittest.TestLoader()
     current_dir = os.path.join(os.path.dirname(__file__))
-    suites = loader.discover(os.path.join(current_dir, "tests"))
+    suite = loader.discover(os.path.join(current_dir, "tests"))
+
+    def flatten(x):
+        l = []
+        try:
+            for s in x:
+                l.extend(flatten(s))
+        except TypeError:
+            l.append(x)
+        return l
+
+    suites = [t for t in flatten(suite)]
+    if args.filter is not None:
+        suites = [t for t in suites if args.filter in type(t).__name__]
 
     run = unittest.TextTestRunner(
         verbosity=2, failfast=args.exitfirst).run(unittest.TestSuite(suites))
