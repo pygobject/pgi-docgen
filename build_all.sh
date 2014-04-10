052cdc1dbeb3a1e495d0089d3b5a1cf03b573ac0
Christoph Reiter <reiter.christoph@gmail.com>
2014-04-10 02:50:02 +0200

    update gir list to work with current debian sid

--- build_all.sh
+++ build_all.sh
@@ -26,19 +26,19 @@ python ./pgi-docgen.py -f _docs Atk-1.0 Cogl-1.0 CoglPango-1.0 DBus-1.0 \
     Caribou-1.0 Champlain-0.12 Cheese-3.0 Colord-1.0 ColordGtk-1.0 \
     ColorHug-1.0 CryptUI-0.0 Dbusmenu-0.4 DbusmenuGtk3-0.4 Dee-1.0 \
     EBook-1.2 EBookContacts-1.2 EDataServer-1.2 Evd-0.1 EvinceDocument-3.0 \
-    EvinceView-3.0 Gck-1 GConf-2.0 Gcr-3 GcrUi-3 GData-0.0 Gdl-3 Gdm-1.0 \
-    GeocodeGlib-1.0 GExiv2-0.4 Gkbd-3.0 GMenu-3.0 GnomeBluetooth-1.0 \
+    EvinceView-3.0 Gck-1 GConf-2.0 GData-0.0 Gdl-3 Gdm-1.0 \
+    GeocodeGlib-1.0 GExiv2-0.10 Gkbd-3.0 GMenu-3.0 GnomeBluetooth-1.0 \
     GnomeDesktop-3.0 GnomeKeyring-1.0 Goa-1.0 Gsf-1 Farstream-0.2 \
     GSSDP-1.0 GtkChamplain-0.12 GtkClutter-1.0 GtkSource-3.0 GtkSpell-3.0 \
     GtkVnc-2.0 Gucharmap-2.90 GUdev-1.0 GUPnP-1.0 UDisks-2.0 GUPnPAV-1.0 \
-    GUPnPDLNA-2.0 GUPnPDLNAGst-2.0 MPID-3.0 Secret-1  Vte-2.90 \
+    GUPnPDLNA-2.0 GUPnPDLNAGst-2.0 MPID-3.0 Secret-1 Vte-2.90 \
     GUPnPIgd-1.0 GUsb-1.0 GVnc-1.0 GVncPulse-1.0 GWeather-3.0 GXPS-0.1 \
     IBus-1.0 Indicate-0.7 Itl-1.0 Poppler-0.18 Clinica-0.3 Tracker-0.16 \
-    Keybinder-0.0 LangTag-0.5 libisocodes-1.0 Libosinfo-1.0 TrackerMiner-0.16 \
+    Keybinder-0.0 LangTag-0.5 Libosinfo-1.0 TrackerMiner-0.16 \
     LibvirtGConfig-1.0 LibvirtGLib-1.0 LibvirtGObject-1.0 TrackerExtract-0.16 \
     LunarDate-2.0 Mx-1.0 Nautilus-3.0 Nemo-3.0 NetworkManager-1.0 \
     PackageKitGlib-1.0 PackageKitPlugin-1.0 PanelApplet-4.0 WebKit-3.0 \
-    Peas-1.0 PeasGtk-1.0 Polkit-1.0 UPowerGlib-1.0 SecretUnstable-0 \
+    Peas-1.0 PeasGtk-1.0 Polkit-1.0 UPowerGlib-1.0 \
     PolkitAgent-1.0 Rest-0.7 RestExtras-0.7 Rsvg-2.0 UMockdev-1.0 \
     SocialWebClient-0.25 SoupGNOME-2.4 SpiceClientGLib-2.0 GOffice-0.10 \
     SpiceClientGtk-3.0 SugarGestures-1.0 TelepathyGLib-0.12 Wnck-3.0 GES-1.0 \
