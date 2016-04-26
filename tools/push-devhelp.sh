4968652f033f381d3608325b054a2836f1aa07b8
Christoph Reiter <reiter.christoph@gmail.com>
2016-04-26 12:17:22 +0200

    Don't force push to pgi-docs/pgi-docs-devhelp
    Things aren't changing that much anymore and this allows
    us to revert and review changes.

--- tools/push-devhelp.sh
+++ tools/push-devhelp.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
-# Copyright 2014 Christoph Reiter
+# Copyright 2014, 2016 Christoph Reiter
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 
-cd _docs/_build_devhelp
-rm -f README.rst
+set -e
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+BUILD="$DIR/../_docs/_build_devhelp"
+
+rm -rf _temp
+git clone https://github.com/lazka/pgi-docs-devhelp.git _temp
+cd _temp
+
+git rm -r "*";
+cp -r "$BUILD"/* .
+
+# add readme
 echo "This repository was created automatically using https://github.com/lazka/pgi-docgen\n" >> README.rst
 echo "It contains devhelp packages for PyGObject\n\n" >> README.rst
 echo "1) git clone https://github.com/lazka/pgi-docs-devhelp.git ~/.local/share/devhelp/books\n" >> README.rst
 echo "2) devhelp\n" >> README.rst
 echo "To update:\n" >> README.rst
 echo "1) cd ~/.local/share/devhelp/books\n" >> README.rst
-echo "2) git fetch --all\n" >> README.rst
-echo "3) git reset --hard origin/master\n" >> README.rst
-rm -rf .?*
-git init
+echo "2) git pull\n" >> README.rst
+
 git add .
 git commit -m "update"
-git remote add origin https://github.com/lazka/pgi-docs-devhelp.git
-git push --set-upstream origin master --force
+git push
--- tools/push.sh
+++ tools/push.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
-# Copyright 2013 Christoph Reiter
+# Copyright 2013, 2016 Christoph Reiter
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 
-# after ./build_all.sh, call this to force push the build to github pages
+# after ./build-debian.py, call this to force push the build to github pages
 
-cd _docs/_build
-rm -f README.rst
+set -e
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+BUILD="$DIR/../_docs/_build"
+
+rm -rf _temp
+git clone https://github.com/lazka/pgi-docs.git _temp
+cd _temp
+git checkout gh-pages
+
+git rm -r "*";
+cp -r "$BUILD"/* .
+
+# add readme
 echo "This repository was created automatically using https://github.com/lazka/pgi-docgen\n" >> README.rst
 echo "It contains a static website which can be viewed at http://lazka.github.io/pgi-docs/\n" >> README.rst
 echo "It also works offline: http://github.com/lazka/pgi-docs/archive/gh-pages.zip" >> README.rst
-rm -rf .?*
-git init
-git checkout -b gh-pages
+
+# disable jekyll
 touch  .nojekyll
+
 git add .
 git commit -m "update"
-git remote add origin https://github.com/lazka/pgi-docs.git
-git push --force --set-upstream origin gh-pages
+git push
