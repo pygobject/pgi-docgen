d511bf5b736e2fe8e9e214ebba1eb78db12c5c94
Christoph Reiter <reiter.christoph@gmail.com>
2019-07-12 17:32:37 +0200

    Update debian versions before building

--- docker/build-docs.sh
+++ docker/build-docs.sh
@@ -6,4 +6,8 @@ TAG="lazka/pgi-docgen"
 
 sudo docker run --security-opt label=disable \
     --rm  --volume "$(pwd)/..:/home/user/app" \
+    -t "${TAG}" ./pgi-docgen update-debian-info
+
+sudo docker run --security-opt label=disable \
+    --rm  --volume "$(pwd)/..:/home/user/app" \
     -t "${TAG}" ./pgi-docgen create-debian _docs
