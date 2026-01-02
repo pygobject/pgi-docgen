a82a854fce01496da61876f24229b0744833b672
Christoph Reiter <reiter.christoph@gmail.com>
2026-01-02 10:14:03 +0100

    and some more

--- docker/build-docs-sphinx.sh
+++ docker/build-docs-sphinx.sh
@@ -6,4 +6,4 @@ TAG="ghcr.io/pygobject/pgi-docgen:v4"
 
 sudo docker run --security-opt label=disable \
     --rm  --volume "$(pwd)/..:/home/user/app" \
-    -t "${TAG}" pgi-docgen build _docs _docs/_build
+    -t "${TAG}" uv run pgi-docgen build _docs _docs/_build
--- docker/build-docs.sh
+++ docker/build-docs.sh
@@ -6,8 +6,8 @@ TAG="ghcr.io/pygobject/pgi-docgen:v4"
 
 sudo docker run --security-opt label=disable \
     --rm  --volume "$(pwd)/..:/home/user/app" \
-    -t "${TAG}" pgi-docgen update-debian-info
+    -t "${TAG}" uv run pgi-docgen update-debian-info
 
 sudo docker run --security-opt label=disable \
     --rm  --volume "$(pwd)/..:/home/user/app" \
-    -t "${TAG}" pgi-docgen create-debian --cachedir /home/user/_debian_build_cache _docs
+    -t "${TAG}" uv run pgi-docgen create-debian --cachedir /home/user/_debian_build_cache _docs
