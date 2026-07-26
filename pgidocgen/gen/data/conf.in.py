e5baf81e969801e7f7ca57edce6936f43faa7e71
Christoph Reiter <reiter.christoph@gmail.com>
2026-07-26 18:00:35 +0200

    sphinx 7 compat

--- pgidocgen/gen/data/conf.in.py
+++ pgidocgen/gen/data/conf.in.py
@@ -50,9 +50,7 @@ html_copy_source = False
 html_show_sourcelink = False
 html_short_title = project = '%s %s' % (mname, mversion)
 html_title = "%s Python API Docs" % html_short_title
-html_context = {
-    "extra_css_files": ["_static/css/pgi.css"],
-}
+html_css_files = ["css/pgi.css"]
 
 
 def linkcode_resolve(domain, info):
